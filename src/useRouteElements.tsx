be5d08d69c7f2eeb4a10e294024e34958d88d584
Minh The <minhthe2804@gmail.com>
2024-10-09 00:49:26 +0700

    Lazyload component

--- src/useRouteElements.tsx
+++ src/useRouteElements.tsx
@@ -1,29 +1,39 @@
+/* eslint-disable react-refresh/only-export-components */
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 
-import ProductList from './pages/ProductList'
-import Login from './pages/Login'
-import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
-import Profile from './pages/User/pages/Profile'
-import { useContext } from 'react'
+import { lazy, Suspense, useContext } from 'react'
 import { AppContext } from './contexts/app.context'
 import { path } from './constants/path'
-import ProductDetail from './pages/ProductDetail'
-import Cart from './pages/Cart'
 import CartLayout from './layouts/CartLayout'
 import UserLayout from './pages/User/layouts/UserLayout'
-import ChangePassword from './pages/User/pages/ChangePassword'
-import HistoryPurchase from './pages/User/pages/HistoryPurchase'
-import NotFound from './pages/NotFound'
 
-// eslint-disable-next-line react-refresh/only-export-components
+// import ProductList from './pages/ProductList'
+// import Login from './pages/Login'
+// import Register from './pages/Register'
+// import Profile from './pages/User/pages/Profile'
+// import ProductDetail from './pages/ProductDetail'
+// import Cart from './pages/Cart'
+// import ChangePassword from './pages/User/pages/ChangePassword'
+// import HistoryPurchase from './pages/User/pages/HistoryPurchase'
+// import NotFound from './pages/NotFound'
+
+const Login = lazy(() => import('./pages/Login'))
+const ProductList = lazy(() => import('./pages/ProductList'))
+const Profile = lazy(() => import('./pages/User/pages/Profile'))
+const Register = lazy(() => import('./pages/Register'))
+const ProductDetail = lazy(() => import('./pages/ProductDetail'))
+const Cart = lazy(() => import('./pages/Cart'))
+const ChangePassword = lazy(() => import('./pages/User/pages/ChangePassword'))
+const HistoryPurchase = lazy(() => import('./pages/User/pages/HistoryPurchase'))
+const NotFound = lazy(() => import('./pages/NotFound'))
+
 function ProtectedRoute() {
     const { isAuthenticated } = useContext(AppContext)
     return isAuthenticated ? <Outlet /> : <Navigate to={path.login} />
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
 function RejectedRoute() {
     const { isAuthenticated } = useContext(AppContext)
     return !isAuthenticated ? <Outlet /> : <Navigate to={path.home} />
@@ -36,7 +46,9 @@ export default function useRouteElements() {
             index: true,
             element: (
                 <MainLayout>
-                    <ProductList />
+                    <Suspense>
+                        <ProductList />
+                    </Suspense>
                 </MainLayout>
             )
         },
@@ -44,7 +56,9 @@ export default function useRouteElements() {
             path: path.productDetail,
             element: (
                 <MainLayout>
-                    <ProductDetail />
+                    <Suspense>
+                        <ProductDetail />
+                    </Suspense>
                 </MainLayout>
             )
         },
@@ -56,7 +70,9 @@ export default function useRouteElements() {
                     path: path.cart,
                     element: (
                         <CartLayout>
-                            <Cart />
+                            <Suspense>
+                                <Cart />
+                            </Suspense>
                         </CartLayout>
                     )
                 },
@@ -64,21 +80,35 @@ export default function useRouteElements() {
                     path: path.user,
                     element: (
                         <MainLayout>
-                            <UserLayout />
+                            <Suspense>
+                                <UserLayout />
+                            </Suspense>
                         </MainLayout>
                     ),
                     children: [
                         {
                             path: path.profile,
-                            element: <Profile />
+                            element: (
+                                <Suspense>
+                                    <Profile />
+                                </Suspense>
+                            )
                         },
                         {
                             path: path.changePassword,
-                            element: <ChangePassword />
+                            element: (
+                                <Suspense>
+                                    <ChangePassword />
+                                </Suspense>
+                            )
                         },
                         {
                             path: path.historyPurchase,
-                            element: <HistoryPurchase />
+                            element: (
+                                <Suspense>
+                                    <HistoryPurchase />
+                                </Suspense>
+                            )
                         }
                     ]
                 }
@@ -92,7 +122,9 @@ export default function useRouteElements() {
                     path: path.login,
                     element: (
                         <RegisterLayout>
-                            <Login />
+                            <Suspense>
+                                <Login />
+                            </Suspense>
                         </RegisterLayout>
                     )
                 },
@@ -100,7 +132,9 @@ export default function useRouteElements() {
                     path: path.register,
                     element: (
                         <RegisterLayout>
-                            <Register />
+                            <Suspense>
+                                <Register />
+                            </Suspense>
                         </RegisterLayout>
                     )
                 }
@@ -110,7 +144,9 @@ export default function useRouteElements() {
             path: '*',
             element: (
                 <MainLayout>
-                    <NotFound />
+                    <Suspense>
+                        <NotFound />
+                    </Suspense>
                 </MainLayout>
             )
         }
